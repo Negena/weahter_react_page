d532c59e441ebdaaed2a89596b5168f6b94032ad
Nigina Muhammadieva <niginamuhammadieva@MacBook-Pro-Nigina.local>
2024-06-12 23:42:04 +0500

    m3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/weather_react_page/',
+  base: '/weather_react_pages/',
 })
